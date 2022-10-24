09d10352619de7145e93ac697e5bffd8237cc89b
“PonceGL” <poncianogl@hotmail.com>
2022-10-24 15:42:28 -0500

    Init Basic Router

---
+++ src/users.py
@@ -0,0 +1,16 @@
+users = [
+  {
+    "userName": "Carlos",
+    "email": "carlos@mail.com",
+    "password": "124567890",
+    "role": "admin",
+    "permissionsId": 1
+  },
+  {
+    "userName": "PonceGL",
+    "email": "poncegl@correo.com",
+    "password": "124567890",
+    "role": "technical support",
+    "permissionsId": 2
+  }
+]
